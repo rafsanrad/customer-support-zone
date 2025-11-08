0bb6a8f552e63d92fabcca560f79ebc500d5f392
rafsanrad <rafsanjahink@gmail.com>
2025-11-08 16:42:04 +0600

    added navbar and footer section

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-black text-[#A1A1AA] ">
+      <div className="w-11/12 mx-auto grid grid-cols-5 gap-10 p-5">
+        <div className="space-y-3">
+          <h3 className="font-bold text-lg !text-white">CS — Ticket System</h3>
+          <p >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type.
+          </p>
+        </div>
+        <div className="space-y-5">
+          <h3 className="font-semibold text-lg !text-white">Company</h3>
+          <p>About Us</p>
+          <p>Our Mission</p>
+          <p>Contact Salad</p>
+        </div>
+        <div className="space-y-5">
+          <h3 className="font-semibold text-lg !text-white">Services</h3>
+          <p>Products & Services</p>
+          <p>Customer Stories</p>
+          <p>Download Apps</p>
+        </div>
+        <div className="space-y-5">
+          <h3 className="font-semibold text-lg !text-white">Information</h3>
+          <p>Privacy Policy</p>
+          <p>Terms & Conditions</p>
+          <p>Join Us</p>
+        </div>
+        <div className="space-y-5">
+          <h3 className="font-semibold text-lg !text-white ">Social Links</h3>
+          <p>@CS — Ticket System</p>
+          <p>@CS — Ticket System</p>
+          <p>@CS — Ticket System</p>
+          <p>support@cst.com</p>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <p className="text-center p-5">© 2025 CS — Ticket System. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div className='w-11/12 mx-auto '>
-            <div>
-                <h3 className='font-bold text-2xl'>CS-Ticket System</h3>
-            </div>
-            <div class>
-                <a>Home</a>
-                <a>FAQ</a>
-                <a>Changelog</a>
-                <a>Blog</a>
-                <a>Download</a>
-                <a>Contact</a>
-                <button>+ New Ticket</button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="shadow-md border-b-[0.5px] border-gray-300">
+        <nav className="px-2 py-3 flex md:flex-row flex-col justify-between items-center w-11/12 mx-auto">
+          <h1 className="text-2xl font-bold">CS-Ticket System</h1>
+          <div className="flex md:mt-0 mt-4 md:flex-row flex-col items-center gap-4">
+            <a href="#" className="text-gray-600">
+              Home
+            </a>
+            <a href="#" className="text-gray-600">
+              FAQ
+            </a>
+            <a href="#" className="text-gray-600">
+              Changelog
+            </a>
+
+            <a href="#" className="text-gray-600">
+              Blog
+            </a>
+            <a href="#" className="text-gray-600">
+              Download
+            </a>
+            <a href="#" className="text-gray-600">
+              Contact
+            </a>
+            <button
+              className=" text-white px-4 py-2 rounded-lg hover:cursor-pointer"
+              style={{
+                background:
+                  "linear-gradient(#632EE3, #9F62F290)"
+              }}
+            >
+              + New Ticket
+            </button>
+          </div>
+          <div className="block md:hidden"></div>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
