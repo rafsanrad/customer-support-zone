f82da98e80575e2f9f9f74ef413e28a828a903b3
rafsanrad <rafsanjahink@gmail.com>
2025-11-16 00:24:14 +0600

    added customerCard

---
+++ src/Components/Cards/CustomerCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const CustomerCard = ({customer}) => {
+    return (
+       <div 
+    // onClick={()=>handleOrder(order)} 
+      className="p-5 rounded-xl shadow-xl relative space-y-3">
+      <div className='flex justify-between items-center'>
+        <h2 className="text-lg flex items-center gap-3">
+        {customer.title} -{customer.description}
+        </h2>
+        <p>{customer.status}</p>
+      </div>
+      <p className="text-normal text-gray-400 ">
+        {customer.description}
+      </p>
+
+      <div className='flex justify-between '>
+        <div className='flex gap-2'>
+            <p>{customer.id}</p>
+            <p>{customer.priority}</p>
+        </div>
+        <div className='flex gap-2'>
+            <p>{customer.customer}</p>
+            <p>{customer.createdAt}</p>
+        </div>
+      </div>
+      
+
+    </div>
+
+    );
+};
+
+export default CustomerCard;
--- src/Components/CustomerContainer.jsx
+++ src/Components/CustomerContainer.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
-import States from './States';
+import React, { use, useState } from "react";
+import States from "./States";
+import CustomerCard from "./Cards/CustomerCard";
 
-const CustomerContainer = ({promise}) => {
-    console.log(promise)
-    return (
-        <div>
-            <States></States>
+const CustomerContainer = ({ promise }) => {
+  
+  const data = use(promise);
+  const [customers,setCustomers]=useState(data)
+  return (
+    <div>
+      <States></States>
+
+      <section className="w-11/12 mx-auto py-10 grid grid-cols-1 md:grid-cols-12 gap-5">
+        <div className=" md:col-span-9">
+          <h2 className="font-bold text-3xl">Customer Tickets</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 space-y-5">
+            {customers.map((customer) => (
+              <CustomerCard
+                // handleOrder={handleOrder}
+                key={customer.id}
+                customer={customer}
+              ></CustomerCard>
+            ))}
+          </div>
         </div>
-    );
+        {/* <div className="md:col-span-5 space-y-5">
+          <h2 className="font-bold text-2xl">Cooking Now</h2>
+          <div className="shadow p-10 space-y-5">
+            {cookingItems.map((order) => (
+              <CookingCard
+                handleCooking={handleCooking}
+                key={order.id}
+                order={order}
+              ></CookingCard>
+            ))}
+          </div>
+          <h2 className="font-bold text-2xl">Order Ready</h2>
+          <div className="shadow p-10 space-y-5">
+            {readyItems.map((order) => (
+              <ReadyCard key={order.id} order={order}></ReadyCard>
+            ))}
+          </div>
+        </div> */}
+      </section>
+    </div>
+  );
 };
 
 export default CustomerContainer;
